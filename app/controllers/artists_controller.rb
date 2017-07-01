2d457530aace1b657819defcbfea9dfc3426d098
tynahuynh <tynahuynh@gmail.com>
2017-06-30 17:18:55 -0700

    html css

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,33 @@
+class ArtistsController < ApplicationController
+	def index
+	RSpotify.authenticate("274c263d612142c483a5989ea1528a21", "0b9514e562874d2897d796259dbbf858")
+
+   if !params[:artist_name].empty?
+   	@arr_artist = params[:artist_name].split(',')
+
+   	@artists_tracks = @arr_artist.map { |artist| RSpotify::Artist.search(artist).first.top_tracks(:US) }
+
+   	# @albums = @arr_artist.map { |artist| RSpotify::Artist.search(artist).first.albums }
+
+   	# @albums_arr = @albums.flatten
+
+   	# @tracks = @albums_arr.map { |album| album.tracks}
+
+   	@tracks_arr = @artists_tracks.flatten
+   	@tracks_id = @tracks_arr.map { |track| track.id }
+
+   	@random_tracks = @tracks_id.shuffle.first(30)
+
+   	@string = @random_tracks.join(",")
+
+   	@site = "https://embed.spotify.com/?theme=white&uri=spotify:trackset:My Playlist:"+ @string
+
+
+   else
+     redirect_to root_path
+   end
+
+
+
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+ get '/artists', to: 'artists#index'
+ get '/artists/:id', to:'artists#show', as: 'artist'
+
+	root 'static_pages#index'
+	get '/auth/spotify/callback', to: 'users#spotify'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
